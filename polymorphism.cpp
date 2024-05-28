05f72055bfdededc21b89d27ae793609ae91aa89
wildanindi <wildanindi2@gmail.com>
2024-05-28 18:17:46 +0700

    menambah class dengan memanggil class seseorang

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -9,3 +9,18 @@ class seseorang {
         //}
 };
 
+class joko :public seseorang {
+    public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang {
+    public:
+    void pesan() {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+
