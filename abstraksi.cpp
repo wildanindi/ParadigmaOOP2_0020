5752039aaddd9fafd2d6d6fc74d4424ac3981812
wildanindi <wildanindi2@gmail.com>
2024-05-28 18:55:17 +0700

    menambah member atau method yang di beri akses modifier public

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -4,5 +4,14 @@ using namespace std;
 class AbtraksiKlas {
 private: string x, y;
 	   
-}
+public:
+
+	//method untuk mengisi nilai
+	//private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	
+};
 
