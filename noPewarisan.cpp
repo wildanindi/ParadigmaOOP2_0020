096dfbc4e17fe645c69be82351e367254b00c585
wildanindi <wildanindi2@gmail.com>
2024-05-28 18:48:30 +0700

    menambahkan final pada method perkenalan

---
+++ noPewarisan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+    public:
+        virtual void perkenalan() final {
+            cout << "Hallo saya Function dari base class";
+        }
+};
+
+class derivedClass : public baseClass {
+    public:
+        void perkenalan() {
+            cout << "Hallo saya Function dari derived class";
+        }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
